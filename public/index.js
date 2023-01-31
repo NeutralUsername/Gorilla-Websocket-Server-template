a89be63ef41ab9997dbcbafe3626dba89b5f6e57
fabian <fabian@fabian.at>
2023-01-31 11:05:10 +0100

    golang webserver min example

--- public/index.js
+++ public/index.js
@@ -1,33 +1,16 @@
 var ws = new WebSocket("ws://"+ window.location.hostname +":8080/ws")
-const MSG_DELIMITER = "<;>"
 
 class Index extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {}
-        this.messageHandler = this.messageHandler.bind(this)
         ws.onmessage = (event) => {
-            this.messageHandler(event.data)
+            if(event.data == "ping")
+                ws.send("pong")
         };
     }   
-    messageHandler(message) {
-        console.log(message)
-        let data = message.split(MSG_DELIMITER);
-        if(data[0] == "request_credentials") {
-            ws.send("credentials"+MSG_DELIMITER+cookie("id")+ MSG_DELIMITER + cookie("name")+ MSG_DELIMITER +cookie("password"));
-        }
-        if(data[0] == "user_data") {
-            let userData = JSON.parse(data[1])
-            document.cookie = "id="+userData.id
-            document.cookie = "name="+userData.name
-            document.cookie = "password="+userData.password
-            this.setState(userData)
-        }
-    }
     render() {
-        if(this.state.id)
-            return React.createElement("div", {}, "Hello "+ this.state.name)
-        else return React.createElement("div", {}, "authenticating...")
+        return React.createElement("div", {}, "hello world")
     }
 }
 
@@ -37,10 +20,3 @@ function start_app() {
     )
   }
 start_app();
-
-
-function cookie(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
-    if (parts.length == 2) return parts.pop().split(";").shift();
-}
--- server.go
+++ server.go
@@ -2,74 +2,62 @@ package main
 
 import (
 	"log"
-	"math/rand"
 	"net/http"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/gorilla/websocket"
 )
 
-var activeUsersSync = sync.Map{}
-var activeSocketsSync = sync.Map{}
-
-const VALID_NAME_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_" //valid characters for usernames and ids
 const MSG_DELIMITER = "<;>"
+const PORT = ":8080"
+
+var SocketToUserSyncMap = sync.Map{}
+var ActiveSocketCount = 0
+
+type ActiveUser struct {
+	Id          string   //unchangable unique id for user USERID_LENGTH random characters a-z, A-Z, 0-9, _
+	SocketCount int      //number of websockets user is connected with
+	Websockets  sync.Map //websockets user is connected with
+}
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	connectToDB()
-	dropTables()
-	initDB()
 	http.HandleFunc("/", servePublic)      //serve files from public folder on root
 	http.HandleFunc("/ws", serveWebsocket) //serve websocket connections
 
-	err := http.ListenAndServe(":8080", nil) //start server on port 8080
+	log.Println("http/websocket server started on " + PORT)
+	err := http.ListenAndServe(PORT, nil) //start server on PORT
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	log.Println("http server started on :8080")
 }
+
 func servePublic(w http.ResponseWriter, r *http.Request) {
 	http.FileServer(http.Dir("./public")).ServeHTTP(w, r) //serve files from public folder
 }
+
 func serveWebsocket(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil) // upgrade http connection to websocket
-	if err == nil {                          //if upgrade was successful
-		conn.WriteMessage(1, constructMessage("request_credentials", []string{})) //request credentials from client. the application is based on conn being linked to a user
-		for {                                                                     //loop while connection is open.
-			_, message, err := conn.ReadMessage() //read message from client
-			if err == nil {                       //if no error
+	if conn, err := upgrader.Upgrade(w, r, nil); err == nil { //if upgrade was successful
+		conn.WriteMessage(ConstructMessage("ping", []string{})) //request credentials from client. the application is based on conn being linked to a user
+		for {                                                   //loop while connection is open.
+			if _, message, err := conn.ReadMessage(); err == nil { //if no error
 				messageHandler(conn, message) //handle message
-			} else { //if error
-				break //break loop and close connection
+			} else {
+				break //break loop if error
 			}
 		}
 		defer func() { //deferred function to run when function ends
-			logout(conn) //logout user and close connection
+			conn.Close() //close connection
 		}()
 	}
 }
 
 func messageHandler(conn *websocket.Conn, message []byte) { //handle messages from client
 	segments := strings.Split(string(message), MSG_DELIMITER) //split message into segments
-	message_type := segments[0]                               //first segment is the message type and the rest are the message data
-	if message_type == "credentials" {                        //if message type is credentials
-		login(conn, segments) //login user
+	switch segments[0] {                                      //switch on message type
+	case "pong":
+		println("pong")
 	}
-	if message_type == "placeholder" {
-		println("testing placeholder implementation")
-	}
-}
-
-func constructMessage(message_type string, message_data []string) []byte { //construct message from message type and message data
-	message := message_type + MSG_DELIMITER //create message
-	for _, data := range message_data {     //add message data
-		message += data + MSG_DELIMITER
-	}
-	return []byte(message) //return message
 }
 
 var upgrader = websocket.Upgrader{
@@ -78,4 +66,12 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
+}
+
+func ConstructMessage(message_type string, message_data []string) (int, []byte) { //construct message from message type and message data
+	message := message_type             //create message
+	for _, data := range message_data { //add message data
+		message += MSG_DELIMITER + data
+	}
+	return 1, []byte(message) //return message
 }
